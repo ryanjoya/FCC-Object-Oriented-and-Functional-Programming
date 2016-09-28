e9bdeebbee108f9b18b2e6ca5261d836b87bab4d
Ryan <ryanjoya@users.noreply.github.com>
2016-09-27 20:05:46 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
 
-var Car = function() {
-  this.wheels = 4;
-  this.engines = 1;
-  this.seats = 5;
+var Car = function(wheels, seats, engines) {
+  //Change this constructor
+  this.wheels = wheels;
+  this.seats = seats;
+  this.engines = engines;
 };
 
-// Only change code below this line.
-
-var myCar = new Car();
-myCar.nickname = "Hot Rod";
+//Try it out here
+var myCar = new Car(6, 3, 1);
