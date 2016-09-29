aef210a937536e18394bd13699722d0056dca5d9
Ryan <ryanjoya@users.noreply.github.com>
2016-09-28 23:34:40 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,37 @@
 
-var Car = function(wheels, seats, engines) {
-  //Change this constructor
-  this.wheels = wheels;
-  this.seats = seats;
-  this.engines = engines;
+var Car = function() {
+  // this is a private variable
+  var speed = 10;
+
+  // these are public methods
+  this.accelerate = function(change) {
+    speed += change;
+  };
+
+  this.decelerate = function() {
+    speed -= 5;
+  };
+
+  this.getSpeed = function() {
+    return speed;
+  };
 };
 
-//Try it out here
-var myCar = new Car(6, 3, 1);
+var Bike = function() {
+
+  // Only change code below this line.
+  var gear = 4;
+  
+  this.setGear = function(set) {
+    gear = set;
+  };
+  
+  this.getGear = function() {
+    return gear;
+  };
+
+};
+
+var myCar = new Car();
+
+var myBike = new Bike();
