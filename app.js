3fb91dee7f3b2b72600a0315f22cf8fd34b5b057
Ryan <ryanjoya@users.noreply.github.com>
2016-10-05 15:44:25 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 
-var oldArray = [1,2,3];
-var newArray = [];
-
-var concatMe = [4,5,6];
+var string = "Split me into an array";
+var array = [];
 
 // Only change code below this line.
 
-newArray = oldArray.concat(concatMe);
+var array = string.split(' ');
